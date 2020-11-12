dac1d45fe37595602576afedee9a0bb1dfc233f3
TrevorFlanigan <trevorflanigan@alumni.ubc.ca>
2020-11-11 23:31:46 -0800

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./server";
-
-app.listen(4000, () => {
-  console.log("Server Started on port 4000");
+let port = process.env.port || 4000;
+app.listen(port, () => {
+  console.log(`Server Started on port ${port}`);
 });
